1cac0630aa2eeb8ddfcc0907c0fb095ef5669eb8
Your Name <sukhdevsingh6369@gmail.com>
2021-06-14 20:30:36 +0800

    fixed bug with product ref still in urls.py

--- tigerears2/urls.py
+++ tigerears2/urls.py
@@ -17,13 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 from pages.views import home_view, contact_view, about_view
-from products.views import (
-    product_create_view, 
-    product_detail_view, 
-    product_delete_view,
-    product_list_view,
-    product_update_view,
-)
+
 
 
 urlpatterns = [
